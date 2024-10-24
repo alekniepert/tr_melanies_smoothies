83c368946d08565cf51fedd4aaf1d788d75b2c75
alekniepert <140488471+alekniepert@users.noreply.github.com>
2024-10-24 14:49:17 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ time_to_insert = st.button('Submit Order')
 if time_to_insert:
     session.sql(my_insert_stmt).collect()
     
-    st.success('Your Smoothie is ordered!', icon="✅")
+    st.success(f'Your Smoothie is ordered,{name_on_order}', icon="✅")
 
 
 
